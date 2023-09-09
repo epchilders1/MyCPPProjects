1890e385e5f8e71a426049af32bc7b98c9a7ce12
epchilders1 <114449117+epchilders1@users.noreply.github.com>
2023-09-08 22:25:43 -0400

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout << "What is your name?"<< endl;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+
+    return 0;
+}
